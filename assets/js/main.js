36428dbe37a7f5f16d963a9aea7276d11abaf06a
Danish Rangaiz <idanishrangaiz@gmail.com>
2022-09-06 22:23:50 +0500

    dsascx

--- assets/js/main.js
+++ assets/js/main.js
@@ -345,14 +345,14 @@ $('.popupimg').magnificPopup({
   zoom: {
     enabled: true, 
 
-    duration: 300, // duration of the effect, in milliseconds
+    duration: 100, // duration of the effect, in milliseconds
     easing: 'ease-in-out', // CSS transition easing function
 
     opener: function(openerElement) {
 
       return openerElement.is('img') ? openerElement : openerElement.find('img');
   }
-}
+
 
 });
 
