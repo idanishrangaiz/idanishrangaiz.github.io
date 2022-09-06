9148710c0f553369e748a1c1fb0b0cbf1de3a652
Danish Rangaiz <idanishrangaiz@gmail.com>
2022-09-06 17:06:11 +0500

    sffd

--- assets/js/main.js
+++ assets/js/main.js
@@ -340,29 +340,29 @@ $('.isotope-pager').on( 'click', 'a', function() {
   
   
 
-$(document).ready(function(){
-$('.popupimg').magnificPopup({
-  type: 'image',
-  mainClass: 'mfp-with-zoom', 
-  gallery:{
-      enabled:true
-    },
+// $(document).ready(function(){
+// $('.popupimg').magnificPopup({
+//   type: 'image',
+//   mainClass: 'mfp-with-zoom', 
+//   gallery:{
+//       enabled:true
+//     },
 
-  zoom: {
-    enabled: true, 
+//   zoom: {
+//     enabled: true, 
 
-    duration: 300, // duration of the effect, in milliseconds
-    easing: 'ease-in-out', // CSS transition easing function
+//     duration: 300, // duration of the effect, in milliseconds
+//     easing: 'ease-in-out', // CSS transition easing function
 
-    opener: function(openerElement) {
+//     opener: function(openerElement) {
 
-      return openerElement.is('img') ? openerElement : openerElement.find('img');
-  }
-}
+//       return openerElement.is('img') ? openerElement : openerElement.find('img');
+//   }
+// }
 
-});
+// });
 
-});
+// });
 
 
 
