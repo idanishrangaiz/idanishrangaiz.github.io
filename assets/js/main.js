66d60a9694c0e53763ba48623814540f8e07a1e6
Danish Rangaiz <idanishrangaiz@gmail.com>
2022-09-07 23:14:29 +0500

    dsdfds

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,19 +117,25 @@
   });
 
   // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
+  $(window).on('load', function() { 
+    setTimout(function(){
+      var portfolioIsotope = $('.portfolio-container').isotope({
+          itemSelector: '.portfolio-item',
+          layoutMode: 'fitRows'
+        });
+        $('#portfolio-flters li').on('click', function() {
+          $("#portfolio-flters li").removeClass('filter-active');
+          $(this).addClass('filter-active');
+          portfolioIsotope.isotope({
+            filter: $(this).data('filter')
+          });
+        });
+        // Initiate venobox (lightbox feature used in portofilo)
+        $(document).ready(function() {
+          $('.venobox').venobox();
+        });
+    },500);
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
     });
 
     // Initiate venobox (lightbox feature used in portofilo)
